c80987b77c57cb6d85b2b37f7d42114d1ac059d5
Gekayos <127081727+Gekayos@users.noreply.github.com>
2023-04-01 07:46:34 -0400

    Add files via upload

---
+++ const.js
@@ -0,0 +1,36 @@
+
+
+
+var PI = 3.14;
+
+
+//#1 
+
+const PI = 3.14;
+
+
+// #2
+
+/* 
+ the difference between var and let is that you can redeclare as well as reassign "var", but you cant redeclare "let", you can also access a variable with "var"
+*/
+
+// #3
+
+/*
+ the difference between var and const is that you can redeclare as well as reassign "var", but you cant redeclare  or reassign "const", you can also access a variable with var
+*/
+
+
+// #4
+
+/*
+the difference between let and const is that you can  reassign "let", but you cant redeclare  or reassign "const"
+*/
+
+
+// #5
+
+/*
+Hoisting is how the JS compiler functions, when you declare a variable it is hoisted to the top of the scope
+*/
